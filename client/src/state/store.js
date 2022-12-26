32f7ea0ea5a20e8e5273bbd1083fbbb5dd98731d
shahid1223 <ms413959@gmail.com>
2022-12-26 08:37:20 +0530

    second commit

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import detailsSlice from './details/detailsSlice';
 import todoSlice from './todo/todoSlice';
 
 const store = configureStore({
     reducer: {
-        detail: detailsSlice,
         todo: todoSlice
     },
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 export default store
